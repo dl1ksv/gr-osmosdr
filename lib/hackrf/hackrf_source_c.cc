1b3e6c1730b8c154cf16caa0d8f16149ec8e72d5
Clayton Smith <argilo@gmail.com>
2016-05-09 08:18:23 -0400

    Handle stopping & restarting to make T/R switching possible.

--- lib/hackrf/hackrf_source_c.cc
+++ lib/hackrf/hackrf_source_c.cc
@@ -210,9 +210,6 @@ hackrf_source_c::hackrf_source_c (const std::string &args)
   }
 
 //  _thread = gr::thread::thread(_hackrf_wait, this);
-
-  ret = hackrf_start_rx( _dev, _hackrf_rx_callback, (void *)this );
-  HACKRF_THROW_ON_ERROR(ret, "Failed to start RX streaming")
 }
 
 /*
@@ -222,12 +219,7 @@ hackrf_source_c::~hackrf_source_c ()
 {
   if (_dev) {
 //    _thread.join();
-    int ret = hackrf_stop_rx( _dev );
-    if ( ret != HACKRF_SUCCESS )
-    {
-      std::cerr << HACKRF_FORMAT_ERROR(ret, "Failed to stop RX streaming") << std::endl;
-    }
-    ret = hackrf_close( _dev );
+    int ret = hackrf_close( _dev );
     if ( ret != HACKRF_SUCCESS )
     {
       std::cerr << HACKRF_FORMAT_ERROR(ret, "Failed to close HackRF") << std::endl;
@@ -294,13 +286,11 @@ bool hackrf_source_c::start()
 {
   if ( ! _dev )
     return false;
-#if 0
   int ret = hackrf_start_rx( _dev, _hackrf_rx_callback, (void *)this );
   if ( ret != HACKRF_SUCCESS ) {
     std::cerr << "Failed to start RX streaming (" << ret << ")" << std::endl;
     return false;
   }
-#endif
   return true;
 }
 
@@ -308,13 +298,11 @@ bool hackrf_source_c::stop()
 {
   if ( ! _dev )
     return false;
-#if 0
   int ret = hackrf_stop_rx( _dev );
   if ( ret != HACKRF_SUCCESS ) {
     std::cerr << "Failed to stop RX streaming (" << ret << ")" << std::endl;
     return false;
   }
-#endif
   return true;
 }
 
