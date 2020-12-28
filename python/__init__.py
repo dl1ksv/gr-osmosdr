ad00551158e4e1b5736f045b44b9079ab62c3cfd
Volker Schroer <3470424+dl1ksv@users.noreply.github.com>
2020-12-28 17:49:23 +0100

    Preparing python bindings for ranges, source and time_spec

--- python/__init__.py
+++ python/__init__.py
@@ -22,4 +22,16 @@
 This is the GNU Radio OsmoSDR module.
 '''
 
-from .osmosdr_swig import *
+from __future__ import unicode_literals
+import os
+
+# import pybind11 generated symbols into the iio namespace
+try:
+    from .osmosdr_python import *
+except ImportError:
+    dirname, filename = os.path.split(os.path.abspath(__file__))
+    __path__.append(os.path.join(dirname, "bindings"))
+    from .osmosdr_python import *
+
+# import any pure python here
+#
---
+++ python/bindings/docstrings/device_pydoc_template.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+#include "pydoc_macros.h"
+#define D(...) DOC(gr,osmosdr, __VA_ARGS__ )
+/*
+  This file contains placeholders for docstrings for the Python bindings.
+  Do not edit! These were automatically extracted during the binding process
+  and will be overwritten during the build process
+ */
+
+
+ 
+ static const char *__doc_gr_osmosdr_device_t = R"doc()doc";
+
+
+ static const char *__doc_gr_osmosdr_device_t_device_t_0 = R"doc()doc";
+
+
+ static const char *__doc_gr_osmosdr_device_t_device_t_1 = R"doc()doc";
+
+
+ static const char *__doc_gr_osmosdr_device_t_to_pp_string = R"doc()doc";
+
+
+ static const char *__doc_gr_osmosdr_device_t_to_string = R"doc()doc";
+
+ 
+ static const char *__doc_gr_osmosdr_device = R"doc()doc";
+
+
+ static const char *__doc_gr_osmosdr_device_device = R"doc()doc";
+
+
+ static const char *__doc_gr_osmosdr_device_find = R"doc()doc";
+
+  
